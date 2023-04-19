98bc7175ec91bcabbb2bda5c111d327b248b2f9f
panashe <76921910+savagenashe@users.noreply.github.com>
2023-04-19 02:04:04 +0000

    Update healthnewsscraper.py

--- healthnewsscraper.py
+++ healthnewsscraper.py
@@ -1,8 +1,10 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
 import requests
 from bs4 import BeautifulSoup
 
 app = Flask(__name__)
+CORS(app)  # enable CORS for all routes
 
 @app.route('/')
 def scrape_news():
