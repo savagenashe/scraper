2eff5874eb7ca346feb3cb69c6d34019177990e6
panashe <76921910+savagenashe@users.noreply.github.com>
2023-04-19 01:12:14 +0000

    Update healthnewsscraper.py

--- healthnewsscraper.py
+++ healthnewsscraper.py
@@ -1,30 +1,35 @@
+from flask import Flask, jsonify
 import requests
 from bs4 import BeautifulSoup
-import json
 
-url = "https://www.afro.who.int/countries/ghana/news"
-response = requests.get(url)
-soup = BeautifulSoup(response.text, "html.parser")
+app = Flask(__name__)
 
-article_containers = soup.find_all("article", class_="news")
-articles = []
+@app.route('/')
+def scrape_news():
+    url = "https://www.afro.who.int/countries/ghana/news"
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "html.parser")
 
-for container in article_containers:
-    title = container.find("h3", class_="teaser-full__title").text.strip()
-    date = container.find("div", class_="date").text.strip()
-    content = container.find("div", class_="content").text.strip()
-    read_more_rel_url = container.find("div", class_="read_more").find("a")["href"]
-    read_more_full_url = "https://www.afro.who.int" + read_more_rel_url
-    
-    article = {
-        "title": title,
-        "date": date,
-        "content": content,
-        "read_more": read_more_full_url
-    }
-    
-    articles.append(article)
+    article_containers = soup.find_all("article", class_="news")
+    articles = []
 
-json_data = json.dumps(articles)
+    for container in article_containers:
+        title = container.find("h3", class_="teaser-full__title").text.strip()
+        date = container.find("div", class_="date").text.strip()
+        content = container.find("div", class_="content").text.strip()
+        read_more_rel_url = container.find("div", class_="read_more").find("a")["href"]
+        read_more_full_url = "https://www.afro.who.int" + read_more_rel_url
 
-print(json_data)
+        article = {
+            "title": title,
+            "date": date,
+            "content": content,
+            "read_more": read_more_full_url
+        }
+
+        articles.append(article)
+
+    return jsonify(articles)
+
+if __name__ == '__main__':
+    app.run()
